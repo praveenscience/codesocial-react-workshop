f3afb31469225f11632ffaf91c17ef69a461034c
Praveen Kumar <praveenscience@gmail.com>
2024-06-08 11:59:21 +0100

    Reorganised and removed useless files.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Code Social React</h1>
+        <p>There are so many people like Gireendra and Tanvi and Shruthi</p>
+      </div>
+    );
+  }
+}
+
+export default App;
